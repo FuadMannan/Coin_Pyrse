67fe2f158c17509d1ab436e397c021adda231f6c
FuadMannan <fuad.mannan@gmail.com>
2022-08-07 18:44:45 -0400

    Implement query class

---
+++ query.py
@@ -0,0 +1,15 @@
+from reporter import *
+
+
+class Query:
+    
+    def __init__(self, name, filter_list):
+        self.name = name
+        self.reporter = Reporter()
+        self.filter_list = filter_list
+        
+    def add_filter(self, search_filter):
+        self.filter_list.append(search_filter)
+
+    def remove_filter(self, search_filter):
+        self.filter_list.remove(search_filter)
